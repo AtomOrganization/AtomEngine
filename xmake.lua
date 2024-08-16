214b2e7039c8c907331f5844eca74911ccc716fa
adriendev <73515932+Adrien35240@users.noreply.github.com>
2024-08-16 11:46:32 +0200

    wip glfw

--- xmake.lua
+++ xmake.lua
@@ -5,9 +5,13 @@ set_defaultmode("debug")
 target("AtomEngine")
     set_kind("static")
     set_languages("cxx20")
+    --add_cxflags("-mmacosx-version-min=14.5")
+    --add_ldflags("-mmacosx-version-min=14.5")
     add_files("src/*.cpp")
-    add_includedirs("include")
-
+    add_includedirs("include","utils")
+    add_links("glfw3")
+    add_linkdirs("lib")
+    add_frameworks("OpenGL")
     -- Download json.lua from AtomUtils, sample for integrate custom script
     --before_build(function (target)
     --    os.exec("curl -L --fail --retry 3 --progress-bar -o json.lua https://raw.githubusercontent.com/AtomOrganization/AtomUtils/main/scripts-lua/json.lua")
@@ -19,7 +23,6 @@ target("AtomEngine")
 after_install(function (package)
     -- Copy headers include folder in package folder
     os.cp("include", package:installdir("include"))
-    os.cp("lib", package:installdir(""))
     -- Copy utils from AtomUtils in package folder
     -- os.cp("utils", package:installdir("utils"))
 end)
